9ce447aadf6a2f3ac46f782404125de14378c085
Khunanon Charoendee <109776878+KKhunanon@users.noreply.github.com>
2023-12-17 00:14:53 +0700

    Commit Fix yarn

--- myApp.js
+++ myApp.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
+module.exports = app;
+const API = require('./server.js'); // Declare 'api' once
 
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', API); // Use 'api' here
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/views/index.html');
+});
 
+let Port = process.env.PORT || 3030;
+
+app.listen(Port, () => {
+  console.log(`Useful Programmer Info Security App Started on Port ${Port}`);
+});
 
 
 
